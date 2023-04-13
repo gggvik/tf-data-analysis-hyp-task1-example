553a29ff8652c99d134d8b146fb9a34a8e212a1c
hhhhh <52508643+gggvik@users.noreply.github.com>
2023-04-13 16:27:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,9 +12,9 @@ def solution(x_success: int,
   p_1 = x_success/x_cnt
   p_2 = y_success/y_cnt
 # Вычисление Z-статистики и p-value
-p_pooled = (p_1 + p_2) / (x_success + y_success)
-z_stat = (p_2 - p_1) / ((p_pooled * (1 - p_pooled) * (1/x_success + 1/y_success)) ** 0.5)
-p_value = norm.sf(abs(z_stat)) * 2
+  p_pooled = (p_1 + p_2) / (x_success + y_success)
+  z_stat = (p_2 - p_1) / ((p_pooled * (1 - p_pooled) * (1/x_success + 1/y_success)) ** 0.5)
+  p_value = norm.sf(abs(z_stat)) * 2
 
 # Определение статистической значимости
-return p_value < 0.03
+  return p_value < 0.03
